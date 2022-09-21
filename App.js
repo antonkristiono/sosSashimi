97a780a0b63a72687d1f61c2514c75611169aa0d
Anton Kristiono <antonkristiono47@gmail.com>
2022-09-21 09:40:18 +0700

    first commit

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import * as React from "react";
+// import { Button, View, Text } from "react-native";
 import {
   Text,
   Link,
@@ -11,75 +12,181 @@ import {
   extendTheme,
   VStack,
   Box,
+  Button,
+  Image,
 } from "native-base";
-import NativeBaseIcon from "./components/NativeBaseIcon";
-import { Platform } from "react-native";
+import "react-native-gesture-handler";
+import { View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { ReactNativeZoomableView } from "@openspacelabs/react-native-zoomable-view";
+import { Audio } from "expo-av";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// Define the config
-const config = {
-  useSystemColorMode: false,
-  initialColorMode: "dark",
-};
+function HomeScreen({ navigation }) {
+  const [sound, setSound] = React.useState();
 
-// extend the theme
-export const theme = extendTheme({ config });
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/sirine.mp3")
+    );
+    setSound(sound);
 
-export default function App() {
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  async function stopSound() {
+    setSound();
+
+    console.log("Stop Sound");
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  return (
+    <Center bg={"blueGray.900"} px={4} flex={1}>
+      <VStack alignItems="center">
+        <Text color="white" fontSize={"xl"} bold>
+          Tekan tombol di bawah ketika
+        </Text>
+        <Text color="white" fontSize={"xl"} bold>
+          perlu{" "}
+          <Text color="#04eee3" fontSize={"xl"} bold>
+            pertolongan
+          </Text>
+        </Text>
+        <Button
+          marginTop={5}
+          borderRadius="full"
+          onPress={() => navigation.navigate("Sos")}
+        >
+          <Image
+            source={require("./assets/1.png")}
+            alt="Alternate Text"
+            size="2xl"
+            // marginTop={7}
+          />
+        </Button>
+        <HStack space={2} alignItems="center">
+          <Button
+            color="emerald.50"
+            marginTop={6}
+            size={"lg"}
+            onPress={() => navigation.navigate("Semilir Map")}
+          >
+            <Text color={"white"} bold>
+              Denah Dusun Semilir
+            </Text>
+          </Button>
+        </HStack>
+        <Button
+          color="emerald.50"
+          marginTop={6}
+          size={"lg"}
+          onPress={stopSound}
+        >
+          <Text color={"white"} bold>
+            Stop Sirine
+          </Text>
+        </Button>
+      </VStack>
+    </Center>
+  );
+}
+
+function Sos() {
+  return (
+    <Center>
+      <View
+        style={{ borderWidth: 1, height: "100%", width: "100%" }}
+      >
+        <Image
+          source={require("./assets/sos.gif")}
+          alt="sos"
+          width="100%"
+          height="100%"
+          resizeMode="contain"
+          style={{
+            transform: [{ rotate: "90deg" }],
+          }}
+
+          // marginTop={7}
+        />
+      </View>
+    </Center>
+  );
+}
+
+function DetailsScreen() {
+  return (
+    // <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    //   <Text>Details Screen</Text>
+    //   <Button
+    //     title="Go to Details... again"
+    //     onPress={() => navigation.push("Details")}
+    //   />
+    //   <Button title="Go to Home" onPress={() => navigation.navigate("Home")} />
+    //   <Button title="Go back" onPress={() => navigation.goBack()} />
+    //   <Button
+    //     title="Go back to first screen in stack"
+    //     onPress={() => navigation.popToTop()}
+    //   />
+    // </View>
+    <Center>
+      <View
+        style={{ borderWidth: 1, flexShrink: 1, height: "100%", width: "100%" }}
+      >
+        <ReactNativeZoomableView
+          // initialZoom={1}
+          maxZoom={30}
+          // contentWidth="100%"
+          // contentHeight="100%"
+        >
+          <Image
+            // style={{ width: "100%", height: "100%", resizeMode: "contain" }}
+            // source={{ uri: "https://via.placeholder.com/400x200.png" }}
+            source={require("./assets/denah.png")}
+            alt="Denah"
+            width="100%"
+            height="100%"
+            resizeMode="contain"
+            // size="2xl"
+          />
+        </ReactNativeZoomableView>
+      </View>
+    </Center>
+  );
+}
+
+const Stack = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Stack.Navigator useLegacyImplementation>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Semilir Map" component={DetailsScreen} />
+      <Stack.Screen name="Sos" component={Sos} />
+    </Stack.Navigator>
+  );
+}
+
+function App() {
   return (
     <NativeBaseProvider>
-      <Center
-        _dark={{ bg: "blueGray.900" }}
-        _light={{ bg: "blueGray.50" }}
-        px={4}
-        flex={1}
-      >
-        <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
-          <ToggleDarkMode />
-        </VStack>
-      </Center>
+      <NavigationContainer>
+        <MyDrawer />
+      </NavigationContainer>
     </NativeBaseProvider>
   );
 }
 
-// Color Switch Component
-function ToggleDarkMode() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  return (
-    <HStack space={2} alignItems="center">
-      <Text>Dark</Text>
-      <Switch
-        isChecked={colorMode === "light"}
-        onToggle={toggleColorMode}
-        aria-label={
-          colorMode === "light" ? "switch to dark mode" : "switch to light mode"
-        }
-      />
-      <Text>Light</Text>
-    </HStack>
-  );
-}
+export default App;
---
+++ Denah.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { ReactNativeZoomableView } from '@openspacelabs/react-native-zoomable-view';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>ReactNativeZoomableView</Text>
+      <View style={{ borderWidth: 5, flexShrink: 1, height: 500, width: 310 }}>
+        <ReactNativeZoomableView
+          maxZoom={30}
+          // Give these to the zoomable view so it can apply the boundaries around the actual content.
+          // Need to make sure the content is actually centered and the width and height are
+          // dimensions when it's rendered naturally. Not the intrinsic size.
+          // For example, an image with an intrinsic size of 400x200 will be rendered as 300x150 in this case.
+          // Therefore, we'll feed the zoomable view the 300x150 size.
+          contentWidth={300}
+          contentHeight={150}
+        >
+          <Image
+            style={{ width: '100%', height: '100%', resizeMode: 'contain' }}
+            source={{ uri: 'https://via.placeholder.com/400x200.png' }}
+          />
+        </ReactNativeZoomableView>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  box: {
+    width: 60,
+    height: 60,
+    marginVertical: 20,
+  },
+});
